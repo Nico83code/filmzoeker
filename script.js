6a4b9288a28b41905a8ee226da95833d04fff596
nicoman@gmail.com <nico.man.codaisseur@gmail.com>
2020-06-26 12:26:52 +0200

    code clean

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-
-
 const filtered = data.Movies.filter((movie) => {
   return movie.Year >= 2014;
 });
@@ -24,134 +22,57 @@ const batman = data.Movies.filter((movie) => {
   return movie.Title.includes("Batman");
 });
 //   console.log("batman", batman);
-const movieList = document
-  .getElementById("movieList")
-  .getElementsByTagName("ul")[0];
+
+//add poster function
+const addPoster = (name) => {
+  movieList.innerHTML = "";
+  return name.forEach((movie) => {
+    const addMoviesToDom = document.createElement("li");
+    const newPoster = document.createElement("img");
+    const newUrl = document.createElement("a");
+    const movieList = document
+      .getElementById("movieList")
+      .getElementsByTagName("ul")[0];
+    movieList.appendChild(addMoviesToDom);
+    addMoviesToDom.appendChild(newUrl);
+    newUrl.setAttribute(
+      "href",
+      `https://www.imdb.com/title/${movie.imdbID}`
+    );
+    newUrl.appendChild(newPoster);
+    newPoster.setAttribute("src", `${movie.Poster}`);
+  });
+}
+
+const movieList = document.getElementById("movieList").getElementsByTagName("ul")[0];
 const navLinks = document.querySelector(".nav_links");
 const navClick = navLinks.addEventListener("click", (event) => {
   switch (event.target.id) {
     case "latest":
-      movieList.innerHTML = "";
-      return filtered.forEach((movie) => {
-        const addMoviesToDom = document.createElement("li");
-        const newPoster = document.createElement("img");
-        const newUrl = document.createElement("a");
-        const movieList = document
-          .getElementById("movieList")
-          .getElementsByTagName("ul")[0];
-        movieList.appendChild(addMoviesToDom);
-        addMoviesToDom.appendChild(newUrl);
-        newUrl.setAttribute(
-          "href",
-          `https://www.imdb.com/title/${movie.imdbID}`
-        );
-        newUrl.appendChild(newPoster);
-        newPoster.setAttribute("src", `${movie.Poster}`);
-      });
+      return addPoster(filtered);
+
     case "avenger":
-      movieList.innerHTML = "";
-      return avengers.forEach((movie) => {
-        const addMoviesToDom = document.createElement("li");
-        const newPoster = document.createElement("img");
-        const newUrl = document.createElement("a");
-        const movieList = document
-          .getElementById("movieList")
-          .getElementsByTagName("ul")[0];
-        movieList.appendChild(addMoviesToDom);
-        addMoviesToDom.appendChild(newUrl);
-        newUrl.setAttribute(
-          "href",
-          `https://www.imdb.com/title/${movie.imdbID}`
-        );
-        newUrl.appendChild(newPoster);
-        newPoster.setAttribute("src", `${movie.Poster}`);
-      });
+      return addPoster(avengers);
 
     case "xmen":
-      movieList.innerHTML = "";
+      return addPoster(xmen);
 
-      return xmen.forEach((movie) => {
-        const addMoviesToDom = document.createElement("li");
-        const newPoster = document.createElement("img");
-        const newUrl = document.createElement("a");
-        const movieList = document
-          .getElementById("movieList")
-          .getElementsByTagName("ul")[0];
-        movieList.appendChild(addMoviesToDom);
-        addMoviesToDom.appendChild(newUrl);
-        newUrl.setAttribute(
-          "href",
-          `https://www.imdb.com/title/${movie.imdbID}`
-        );
-        newUrl.appendChild(newPoster);
-        newPoster.setAttribute("src", `${movie.Poster}`);
-      });
     case "princess":
-      movieList.innerHTML = "";
+      return addPoster(princess);
 
-      return princess.forEach((movie) => {
-        const addMoviesToDom = document.createElement("li");
-        const newPoster = document.createElement("img");
-        const newUrl = document.createElement("a");
-        const movieList = document
-          .getElementById("movieList")
-          .getElementsByTagName("ul")[0];
-        movieList.appendChild(addMoviesToDom);
-        addMoviesToDom.appendChild(newUrl);
-        newUrl.setAttribute(
-          "href",
-          `https://www.imdb.com/title/${movie.imdbID}`
-        );
-        newUrl.appendChild(newPoster);
-        newPoster.setAttribute("src", `${movie.Poster}`);
-      });
     case "batman":
-      movieList.innerHTML = "";
+      return addPoster(batman);
 
-      return batman.forEach((movie) => {
-        const addMoviesToDom = document.createElement("li");
-        const newPoster = document.createElement("img");
-        const newUrl = document.createElement("a");
-        const movieList = document
-          .getElementById("movieList")
-          .getElementsByTagName("ul")[0];
-        movieList.appendChild(addMoviesToDom);
-        addMoviesToDom.appendChild(newUrl);
-        newUrl.setAttribute(
-          "href",
-          `https://www.imdb.com/title/${movie.imdbID}`
-        );
-        newUrl.appendChild(newPoster);
-        newPoster.setAttribute("src", `${movie.Poster}`);
-      });
     case "searchbar":
       movieList.innerHTML = "";
-
-
       const searchBar = document.getElementById("searchbar");
-
       searchBar.addEventListener("keyup", (event) => {
         const searchTerm = event.target.value.toLowerCase();
         const filteredMovies = data.Movies.filter((movie) => {
           return movie.Title.toLowerCase().includes(searchTerm);
         });
         movieList.innerHTML = "";
-        filteredMovies.forEach((movie) => {
-          const addMoviesToDom = document.createElement("li");
-          const newPoster = document.createElement("img");
-          const newUrl = document.createElement("a");
-          const movieList = document
-            .getElementById("movieList")
-            .getElementsByTagName("ul")[0];
-          movieList.appendChild(addMoviesToDom);
-          addMoviesToDom.appendChild(newUrl);
-          newUrl.setAttribute(
-            "href",
-            `https://www.imdb.com/title/${movie.imdbID}`
-          );
-          newUrl.appendChild(newPoster);
-          newPoster.setAttribute("src", `${movie.Poster}`);
-        });
+        addPoster(filteredMovies);
       });
   }
 });
